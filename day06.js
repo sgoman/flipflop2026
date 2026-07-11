83ac4d3e2cc0a65b3cceba53a7600905f803530c
Manuel Sgobino <manuel.sgobino@gmx.net>
2026-07-11 08:01:43 +0200

    day 06: part 2 works for sample but not individual input

--- day06.js
+++ day06.js
@@ -5,7 +5,7 @@ const { fourWayDeltas, getSurrounding, gridCells } = require('./utils.js')
 const parseInput = input => input.split('\n').map(l => l.split(''))
 const posHash = (row, col) => `${row}:${col}`
 
-const work = (grid) => {
+const work = (grid, gears, hasBluetooth) => {
     const dirs = 'LR'
     let q = []
     let v = new Set()
@@ -18,26 +18,41 @@ const work = (grid) => {
         if (!v.has(h)) {
             v.add(h)
             grid[r][c] = d
-            for (const n of getSurrounding(grid, r, c, fourWayDeltas).filter(t => t.tile == '#')) {
-                let nd = dirs[(dirs.indexOf(d) + 1) % dirs.length]
+            const nd = dirs[(dirs.indexOf(d) + 1) % dirs.length]
+            const neighbours = getSurrounding(grid, r, c, fourWayDeltas)
+            for (const n of neighbours.filter(t => gears.indexOf(t.tile) != -1)) {
                 q.push([n.row, n.col, nd])
+            }
+            if (hasBluetooth) {
+                for (const n of neighbours.filter(t => t.tile.charCodeAt(0) >= 97 && t.tile.charCodeAt(0) <= 122)) {
+                    for (const bto of gridCells(grid).filter(t => t.value == String.fromCharCode(n.tile.charCodeAt(0) - 32))) {
+                        for (const gear of getSurrounding(grid, bto.row, bto.col, fourWayDeltas).filter(t => gears.indexOf(t.tile) != -1)) {
+                            q.push([gear.row, gear.col, nd])
+                        }
+                    }
+                }
             }
         }
     }
     return grid
 }
 
-const part1 = input => {
-    const solutions = { "part 1": null, "part 2": null, "part 3": null}
-
-    let p1 = work(parseInput(input))
-    let lights = gridCells(p1).filter(c => c.value == '*')
+const solve = (input, gears, hasBluetooth) => {
+    let grid = work(parseInput(input), gears, hasBluetooth)
+    let lights = gridCells(grid).filter(c => c.value == '*')
     let bin = lights.reduce((b, l) => {
-        const n = getSurrounding(p1, l.row, l.col, fourWayDeltas).filter(c => c.tile == 'L' || c.tile == 'R')
+        const n = getSurrounding(grid, l.row, l.col, fourWayDeltas).filter(c => c.tile == 'L' || c.tile == 'R')
         if (n.length) b += 'LR'.indexOf(n[0].tile)
         return b
     }, '')
-    solutions["part 1"] = parseInt(bin, 2)
+    return parseInt(bin, 2)
+}
+
+const part1 = input => {
+    const solutions = { "part 1": null, "part 2": null, "part 3": null}
+
+    solutions["part 1"] = solve(input, '#', false)
+    solutions["part 2"] = solve(input, '#3', true)
 
     return solutions
 }
