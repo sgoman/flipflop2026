a797f1801278ace9df2c7a51a50cffb10d7e06d5
Manuel Sgobino <manuel.sgobino@gmx.net>
2026-07-12 05:14:42 +0200

    corrected isPrime to exclude 1 as prime

--- utils.js
+++ utils.js
@@ -193,7 +193,7 @@ const range = function* () {
 }
 
 const isPrime = n => {
-    if (n < 1) return false
+    if (n <= 1) return false
     if (n <= 3) return true
     if (n % 2 == 0 || n % 3 == 0) return false
     for (let i = 5; i * i <= n; i += 6) {
